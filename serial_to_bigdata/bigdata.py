88c9a080b9481ac79cd73840cff6dd6a9a6e3ce9
memoiry <memoiry@outlook.com>
2017-03-25 22:20:25 +0800

    trivial MNIST

---
+++ serial_to_bigdata/bigdata.py
@@ -0,0 +1,64 @@
+
+
+import tensorflow as tf
+import numpy as np
+
+'''
+Your dataset.
+'''
+xs = np.linspace(0.0, 8.0, 8000000) # 8-million features
+ys = 0.3*xs-0.8+np.random.normal(scale=0.25, size=len(xs)) # 8-million labels
+
+'''
+Initial guesses, which will be refined by TensorFlow.
+'''
+m_initial = -0.5 # Initial guesses
+b_initial =  1.0
+
+'''
+Define free variables to be solved.
+'''
+m = tf.Variable(m_initial) # Parameters
+b = tf.Variable(b_initial)
+
+'''
+Define placeholders for big data.
+'''
+_BATCH = 8 # Use only eight points at a time.
+xs_placeholder = tf.placeholder(tf.float32, [_BATCH])
+ys_placeholder = tf.placeholder(tf.float32, [_BATCH]) 
+
+'''
+Define the error between the data and the model as a tensor (distributed computing).
+'''
+ys_model = m*xs_placeholder+b # Tensorflow knows this is a vector operation
+total_error = tf.reduce_sum((ys_placeholder-ys_model)**2) # Sum up every item in the vector
+
+'''
+Once cost function is defined, create gradient descent optimizer.
+'''
+optimizer_operation = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(total_error) # Does one step
+
+'''
+Create operator for initialization.
+'''
+initializer_operation = tf.global_variables_initializer()
+
+'''
+All calculations are done in a session.
+'''
+with tf.Session() as session:
+
+	session.run(initializer_operation) # Call operator
+
+	_EPOCHS = 10000 # Number of "sweeps" across data
+	for iteration in range(_EPOCHS):
+		random_indices = np.random.randint(len(xs), size=_BATCH) # Randomly sample the data
+		feed = {
+			xs_placeholder: xs[random_indices],
+			ys_placeholder: ys[random_indices]
+		}
+		session.run(optimizer_operation, feed_dict=feed) # Call operator
+
+	slope, intercept = session.run((m, b)) # Call "m" and "b", which are operators
+	print('Slope:', slope, 'Intercept:', intercept)
---
+++ serial_to_bigdata/serial.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+
+'''
+Your dataset.
+'''
+xs = [ 0.00,  1.00,  2.00, 3.00, 4.00, 5.00, 6.00, 7.00] # Features
+ys = [-0.82, -0.94, -0.12, 0.26, 0.39, 0.64, 1.02, 1.00] # Labels
+
+'''
+Initial guesses, which will be refined by TensorFlow.
+'''
+m_initial = -0.5 # Initial guesses
+b_initial =  1.0
+
+'''
+Define free variables to be solved.
+'''
+m = tf.Variable(m_initial) # Parameters
+b = tf.Variable(b_initial)
+
+'''
+Define the error between the data and the model one point at a time (slow).
+'''
+total_error = 0.0
+for x, y in zip(xs, ys):
+	y_model = m*x + b # Output of the model aka yhat
+	total_error += (y-y_model)**2 # Difference squared - this is the "cost" to be minimized
+
+'''
+Once cost function is defined, create gradient descent optimizer.
+'''
+optimizer_operation = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(total_error) # Does one step
+
+'''
+Create operator for initialization.
+'''
+initializer_operation = tf.global_variables_initializer()
+
+'''
+All calculations are done in a session.
+'''
+with tf.Session() as session:
+
+	session.run(initializer_operation) # Call operator
+
+	_EPOCHS = 10000 # number of "sweeps" across data
+	for iteration in range(_EPOCHS):
+		session.run(optimizer_operation) # Call operator
+
+	slope, intercept = session.run((m, b)) # Call "m" and "b", which are operators
+	print('Slope:', slope, 'Intercept:', intercept)
